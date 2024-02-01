c2160917493f13af0318b04d6545639947c4d2e0
furkanblta <Furkan@FURKANPC>
2024-02-01 14:27:15 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBarkod = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelUrunAd = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelUrunBarkod = new System.Windows.Forms.Label();
+            this.labelUrunFiyat = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(896, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 68);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBarkod
+            // 
+            this.txtBarkod.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBarkod.Location = new System.Drawing.Point(137, 139);
+            this.txtBarkod.Name = "txtBarkod";
+            this.txtBarkod.Size = new System.Drawing.Size(739, 64);
+            this.txtBarkod.TabIndex = 1;
+            this.txtBarkod.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBarkod_KeyDown);
+            this.txtBarkod.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBarkod_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(73, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(23, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ürün Barkod:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(43, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ürün Fiyatı:";
+            // 
+            // labelUrunAd
+            // 
+            this.labelUrunAd.AutoSize = true;
+            this.labelUrunAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelUrunAd.Location = new System.Drawing.Point(232, 113);
+            this.labelUrunAd.Name = "labelUrunAd";
+            this.labelUrunAd.Size = new System.Drawing.Size(163, 31);
+            this.labelUrunAd.TabIndex = 5;
+            this.labelUrunAd.Text = "labelUrunAd";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(22, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 31);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Barkod";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(267, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(478, 58);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Ürünün Fiyatını Gör";
+            // 
+            // labelUrunBarkod
+            // 
+            this.labelUrunBarkod.AutoSize = true;
+            this.labelUrunBarkod.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelUrunBarkod.Location = new System.Drawing.Point(232, 66);
+            this.labelUrunBarkod.Name = "labelUrunBarkod";
+            this.labelUrunBarkod.Size = new System.Drawing.Size(216, 31);
+            this.labelUrunBarkod.TabIndex = 8;
+            this.labelUrunBarkod.Text = "labelUrunBarkod";
+            // 
+            // labelUrunFiyat
+            // 
+            this.labelUrunFiyat.AutoSize = true;
+            this.labelUrunFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelUrunFiyat.Location = new System.Drawing.Point(232, 164);
+            this.labelUrunFiyat.Name = "labelUrunFiyat";
+            this.labelUrunFiyat.Size = new System.Drawing.Size(190, 31);
+            this.labelUrunFiyat.TabIndex = 9;
+            this.labelUrunFiyat.Text = "labelUrunFiyat";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelUrunFiyat);
+            this.groupBox1.Controls.Add(this.labelUrunBarkod);
+            this.groupBox1.Controls.Add(this.labelUrunAd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(31, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(968, 257);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün Bilgileri Ekranı";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(925, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 62);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(400, 495);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "© 2024 - Tüm hakları saklıdır.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1024, 531);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBarkod);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ürün Fiyat Gör";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBarkod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelUrunAd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelUrunBarkod;
+        private System.Windows.Forms.Label labelUrunFiyat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=DB;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelUrunFiyat.Text = "";
+            labelUrunAd.Text = "";
+            labelUrunBarkod.Text = " ";
+            txtBarkod.Clear();
+           this.ActiveControl= txtBarkod;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string barkodNumarasi = txtBarkod.Text.Trim();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // SQL sorgusunu parametre ile oluştur
+                string sqlQuery = "SELECT ITEM_SALE_PRICE, i.ITEM_NAME, b.BARCODE_NUMBER " +
+                                  "FROM ITEMS i " +
+                                  "JOIN BARCODES b ON i.ITEM_NUMBER = b.ITEM_NUMBER " +
+                                  "WHERE b.BARCODE_NUMBER = @BarkodNumarasi AND STATUS=1";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    // Parametre ekleyerek SQL sorgusunu güvenli hale getir
+                    command.Parameters.AddWithValue("@BarkodNumarasi", barkodNumarasi);
+
+                    // Sorguyu çalıştır ve SqlDataReader ile sonuçları oku
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            decimal fiyat = reader.GetDecimal(0);
+                            string urunAdi = reader.GetString(1);
+                            long barkod = reader.GetInt64(2);
+
+                            labelUrunFiyat.Text = $"{fiyat} TL";
+                            labelUrunAd.Text = urunAdi;
+                            labelUrunBarkod.Text = barkod.ToString();
+                            txtBarkod.Clear();
+                            this.ActiveControl = txtBarkod;
+                            txtBarkod.Focus();
+                        }
+                        else
+                        {
+                            connection.Close();
+                            connection.Open();
+                            // İlk sorgu sonuç vermediği durumda diğer tabloyu sorgula
+                            sqlQuery = "SELECT BARCODE_NUMBER, ITEM_NAME, ITEM_SALE_PRICE FROM ITEM_UNKNOWN " +
+                                       "WHERE BARCODE_NUMBER = @BarkodNumarasi";
+
+                            using (SqlCommand secondCommand = new SqlCommand(sqlQuery, connection))
+                            {
+                                secondCommand.Parameters.AddWithValue("@BarkodNumarasi", barkodNumarasi);
+
+                                using (SqlDataReader secondReader = secondCommand.ExecuteReader())
+                                {
+                                    
+                                    if (secondReader.Read())
+                                    {
+                                        decimal fiyat = secondReader.GetDecimal(2);
+                                        string urunAdi = secondReader.GetString(1);
+                                        long barkod = secondReader.GetInt64(0);
+
+
+                                        labelUrunFiyat.Text = $"{fiyat} TL";
+                                        labelUrunAd.Text = urunAdi;
+                                        labelUrunBarkod.Text = barkod.ToString();
+                                        txtBarkod.Clear();
+                                    }
+                                    else
+                                    {
+                                        
+                                       
+                                        //MessageBox.Show("Ürün bulunamadı.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                                        labelUrunFiyat.Text = "";
+                                        labelUrunAd.Text = "Ürün Bulunamadı";
+                                        labelUrunBarkod.Text = txtBarkod.Text;
+                                        txtBarkod.Clear();
+                                        //MessageBox.Show("Ürün bulunamadı.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtBarkod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                string barkodNumarasi = txtBarkod.Text.Trim();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL sorgusunu parametre ile oluştur
+                    string sqlQuery = "SELECT ITEM_SALE_PRICE, i.ITEM_NAME, b.BARCODE_NUMBER " +
+                                      "FROM ITEMS i " +
+                                      "JOIN BARCODES b ON i.ITEM_NUMBER = b.ITEM_NUMBER " +
+                                      "WHERE b.BARCODE_NUMBER = @BarkodNumarasi AND STATUS=1";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        // Parametre ekleyerek SQL sorgusunu güvenli hale getir
+                        command.Parameters.AddWithValue("@BarkodNumarasi", barkodNumarasi);
+
+                        // Sorguyu çalıştır ve SqlDataReader ile sonuçları oku
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                decimal fiyat = reader.GetDecimal(0);
+                                string urunAdi = reader.GetString(1);
+                                long barkod = reader.GetInt64(2);
+
+                                labelUrunFiyat.Text = $"{fiyat} TL";
+                                labelUrunAd.Text = urunAdi;
+                                labelUrunBarkod.Text = barkod.ToString();
+                                txtBarkod.Clear();
+                            }
+                            else
+                            {
+                                connection.Close();
+                                connection.Open();
+                                // İlk sorgu sonuç vermediği durumda diğer tabloyu sorgula
+                                sqlQuery = "SELECT BARCODE_NUMBER, ITEM_NAME, ITEM_SALE_PRICE FROM ITEM_UNKNOWN " +
+                                           "WHERE BARCODE_NUMBER = @BarkodNumarasi";
+
+                                using (SqlCommand secondCommand = new SqlCommand(sqlQuery, connection))
+                                {
+                                    secondCommand.Parameters.AddWithValue("@BarkodNumarasi", barkodNumarasi);
+
+                                    using (SqlDataReader secondReader = secondCommand.ExecuteReader())
+                                    {
+                                        if (secondReader.Read())
+                                        {
+                                            decimal fiyat = secondReader.GetDecimal(2);
+                                            string urunAdi = secondReader.GetString(1);
+                                            long barkod = secondReader.GetInt64(0);
+
+                                            labelUrunFiyat.Text = $"{fiyat} TL";
+                                            labelUrunAd.Text = urunAdi;
+                                            labelUrunBarkod.Text = barkod.ToString();
+                                            txtBarkod.Clear();
+                                        }
+                                        else
+                                        {
+
+                                            //MessageBox.Show("Ürün bulunamadı.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                            labelUrunFiyat.Text = "";
+                                            labelUrunAd.Text = "Ürün Bulunamadı";
+                                            labelUrunBarkod.Text = txtBarkod.Text;
+                                            txtBarkod.Clear();
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtBarkod_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
+
